889fe23fa68602151aa2e44c8cb51e6aca55b9c9
harikapadia999 <harikapadia999@gmail.com>
2023-02-22 15:40:35 +0530

    Fetch movie data from TMDb api in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
-import { Inter } from "@next/font/google";
+import Results from "@/component/Results";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <h1>hari</h1>;
+const API_KEY = process.env.API_KEY;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated " ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
+  return (
+    <div className="">
+      <Results results={results} />
+    </div>
+  );
 }
